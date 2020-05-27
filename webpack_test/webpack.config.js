15b5cc40d5dce6d34ad91c42b94b4aef20a4251a
Sean Cuerpo <seancuerpo@gmail.com>
2020-05-26 21:59:31 -0400

    webpack app w/ sass init

---
+++ webpack_test/webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: "./src/index.html",
+    filename: "./index.html"
+});
+
+const miniCssPlugin = new MiniCssExtractPlugin({
+    filename: "[name].css"
+})
+
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                use: {
+                    loader: "html-loader",
+                    options: { minimize: true }
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                use: {
+                    loader: "file-loader",
+                    options: { outputPath: "images" }
+                }
+            },
+            {
+                test: /\.scss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            }
+        ]
+    },
+    plugins: [ 
+        htmlPlugin, 
+        miniCssPlugin,
+    ]
+} 
