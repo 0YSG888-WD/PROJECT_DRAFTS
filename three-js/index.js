b9cc36b73bd01e4edd88768de55fee86e1082dae
Sean Cuerpo <seancuerpo@gmail.com>
2020-05-27 21:48:29 -0700

    updated script

--- three-js/index.js
+++ three-js/index.js
@@ -1,6 +1,47 @@
 document.addEventListener("DOMContentLoaded", function(){
     console.log('WHITNEY IS BEAUTIFUL!!')
 
-    const scene = new THREE.Scene();
+    let SCREEN_WIDTH = window.innerWidth;
+    let SCREEN_HEIGHT = window.innerHeight;
+    let renderer, scene, camera, cube;
+
+    function init(){
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+
+        canvas = document.getElementById('container');
+        canvas.appendChild(renderer.domElement);
+
+        scene = new THREE.Scene();
+        camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+        camera.position.z = 2
+
+        const geometry = new THREE.BoxGeometry();
+        const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        cube = new THREE.Mesh(geometry, material);
+        scene.add(cube);
+    };
+    function animate(){
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.02;
+        renderer.render(scene, camera)
+    }
+
+    function windowResize(){
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    window.addEventListener('resize', windowResize);
+
+    init();
+    animate();
 
 });
