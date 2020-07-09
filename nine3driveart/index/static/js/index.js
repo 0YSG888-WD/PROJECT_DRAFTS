2dc0e159c217531b9d617c460b20c02527e84114
Sean Cuerpo <seancuerpo@gmail.com>
2020-07-09 13:58:35 -0400

    IMPORTED HTML/CSS/JS INTO DJANGO SERVER PROJECT FOLDER FOR DEPLOYMENT WITH AWS

---
+++ nine3driveart/index/static/js/index.js
@@ -0,0 +1,59 @@
+document.addEventListener("DOMContentLoaded", function(){
+    var mySwiper = new Swiper('.swiper-container', {    
+        speed: 800,
+        direction: 'vertical',
+        mousewheel: true,
+        parallax: true,
+    });
+    let title = document.querySelector(".title");
+    title.addEventListener("click", function(){
+        console.log("WHITNEY IS BEAUTIFUL");
+        let nav = document.getElementById("navigation");
+        if( nav.style.display == "none" ){
+            nav.style.display = "block";
+            nav.style.animationName = "slide-in";
+        } else {
+            nav.style.animationName = "slide-out";
+            setTimeout(function(){
+                nav.style.display = "none";
+            }, 650);
+        }
+    });
+    const controller = new ScrollMagic.Controller({});
+    const pageOneTimeline = gsap.timeline();
+    pageOneTimeline.from("#page-one", {opacity: 0, delay: 0.05});
+    pageOneTimeline.from(".fuck-it", {opacity: 0, x: -50, delay: -0.25});
+    const pageOneScene = new ScrollMagic.Scene({
+        triggerElement: "#page-one",
+        triggerHook: 0.3
+    })
+    .setTween(pageOneTimeline)
+    .addTo(controller);
+    const pageTwoTimeline = gsap.timeline();
+    pageTwoTimeline.from(".page-two-subtext", {opacity: 0, delay: 0.75});
+    const pageTwoScene = new ScrollMagic.Scene({
+        triggerElement: "#page-two",
+        triggerHook: 0.3
+    })
+    .setTween(pageTwoTimeline)
+    .addTo(controller);
+    const pageThreeTimeline = gsap.timeline();
+    pageThreeTimeline.from("#page-three", {opacity: 0, delay: 0.05});
+    const pageThreeScene = new ScrollMagic.Scene({
+        triggerElement: "#page-three",
+        triggerHook: 0.3
+    })
+    .setTween(pageThreeTimeline)
+    .addTo(controller);
+
+    const pageFourTimeline = gsap.timeline();
+    pageFourTimeline.from("#page-four", {opacity: 0, delay: 0.05});
+    pageFourTimeline.from(".starting-over", {opacity: 0, x: -50, delay: -0.25});
+    pageFourTimeline.from(".welcome-message", {opacity: 0, delay: 4});
+    const pageFourScene = new ScrollMagic.Scene({
+        triggerElement: "#page-four",
+        triggerHook: 0.3
+    })
+    .setTween(pageFourTimeline)
+    .addTo(controller);
+});
